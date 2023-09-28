2ff2e8551684118549cbae697d47f6b8f8779927
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2023-09-28 14:30:08 +0300

    create calc (switch case)

---
+++ calc(switch case).cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "input two num: ";
+    float num1,num2;
+    char op;
+    cin >> num1>>num2;
+    cout << "input op: ";
+    cin >> op;
+    switch (op) {
+    case '+':
+        cout << num1 << '+' << num2 << '=' << num1 + num2;
+        break;
+    case '-':
+        cout << num1 <<'-' << num2 << '=' << num1 - num2;
+        break;
+    case '*':
+        cout << num1 <<'*' << num2 << "=" << num1 * num2;
+        break;
+    case '/':
+        cout << num1 << '/' << num2 << '=' << num1 / num2;
+        break;
+    default:
+        cout << "lie";
+       
+    
+    }
+    
+ 
+    
+}
+
+
