5522cb07d86b388fa9fbced8a0607abb71e108cc
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2023-12-07 12:56:57 +0300

    min,max,array

---
+++ min,max,array.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	srand(time(NULL));
+	int min, max;
+	const int size = 15;
+	int summ = 0;
+	cin >> min, max;
+	int array[size] = {};
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = min + rand() % (max + 1 - min);
+		cout << array[i] << ' ';
+		summ = summ + array[i];
+		if (max < array[i])
+		{
+			max = array[i];
+		}
+		if (array[i] < min)
+		{
+			min = array[i];
+		}
+	}
+
+	cout << min;
+	cout << max;
+	cout << summ;
+    
+}
