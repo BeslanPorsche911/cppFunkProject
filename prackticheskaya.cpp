a6702fbdb3f871c8aca1a0538eaf12c362700bc9
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2023-12-28 13:59:18 +0300

    prackticheskaya

---
+++ prackticheskaya.cpp
@@ -0,0 +1,90 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+         /*НОМЕР 1*/
+    //int n1= 25 ,n2= 54, n3= 39, n4= 26;
+    //int a, b;
+    //if (n1 > n2) a = n1;
+    //else
+    //    a = n2;
+    //if (n3 > n4) b = n3;
+    //else
+    //    b = 3;
+    //if (a > b)cout << a << "bolshoe";
+    //else
+    //    cout << b << "bolshoe";
+
+        /*  НОМЕР 2*/
+   /* int diap1, diap2,n;
+    cout << "vvedite diap and n celih number: ";
+    cin >> diap1 >> diap2 >> n;
+    if (diap2 < diap1) swap(diap1, diap2);
+    for (int i = 0; i < n; i++) {
+        cout << diap1 << " ";
+        diap1++;*/
+    /*}*/
+   /* НОМЕР 3*/
+    //float max;
+    //cin >> max;
+    //for (float i = 0; i <= max; i += 0.25) cout << i << " ";
+    
+   /* НОМЕР 4*/
+    //int n, a = 0;
+    //float n2, summ = 0;
+    //cin >> n;
+    //while (n != 0) {
+    //    summ += n;
+    //    cin >> n;
+    //    a++;
+    //}
+    //n2 = summ / a;
+    //cout << "srednee arif: " << n2;
+
+      /*  НОМЕР 5
+    char a;
+    cin >> a;
+    if (a >= 65 && &a <= 90 || a >= 97 && a <= 122) {
+        cout << "english buk";
+    }
+    else cout << "ne english buk";*/
+
+    /*НОМЕР 6,7,9*/
+    /*int const SIZE1 = 10;
+    int array1[SIZE1], array2[SIZE1]; for (int i = 0; i < SIZE1; i++) {
+     array1[i] = 7 + rand() % (18 - 7 + 1);  cout << array1[i] << " ";
+      if (array1[i] % 2 == 0) {   array2[i] = array1[i];
+     cout << array2[i] << " ";  }
+     cout << endl; }
+        cout << endl; 
+        for (int i = 0; i < SIZE1; i++) {
+        if (array1[i] % 2 == 0) {   cout << array1[i] << " ";*/
+ /* } }*/
+  /*НОМЕР10
+  int const SIZE1 = 5, SIZE2 = 4;
+  int arr[SIZE1][SIZE2], max = 0, min = 99, summ = 0, a = 0; for (int i = 0; i < SIZE1; i++) {
+   for (int j = 0; j < SIZE2; j++) {   arr[i][j] = 10 + rand() % (99 - 10 + 1);
+    cout << arr[i][j] << " ";   summ += arr[i][j];
+    if (max < arr[i][j]) {    max = arr[i][j];
+     a = j;   }
+    if (min > arr[i][j]) min = arr[i][j];  }
+   cout << endl; }
+  cout << endl; cout << min << " " << max << " " << summ << " " << a;*/
+  /*НОМЕР 8 
+  int n;
+  cin >> n; for (int i = 0; i < n; i++) {
+   cout << "0 ";
+  } cout << endl;
+  for (int i = 0; i < n - 2; i++) {  cout << "0 ";
+   for (int j = 0; j < n - 2; j++) {   if (i == (n - 2) / 2 || j == (n - 2) / 2) cout << "0 ";
+    else cout << "  ";  }
+   cout << "0 " << endl; }
+  for (int i = 0; i < n; i++) {  cout << "0 ";
+  }*/
+
+}
+
+
+
