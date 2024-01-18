e18bf6350d18af30de0a706a944ef58092a003cb
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2024-01-18 14:26:17 +0300

    Add files via upload

---
+++ 6 zadach.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+using namespace std;
+void pryam(int a, int b) {
+    for (int i = 0; i < b; i++) {
+        for (int j = 0; j < a; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void fakt(int a1, int b1) {
+    for (int i = 1; i <= a1; i++) {
+        b1 *= i;
+    }
+}
+void prNum(int a2) {
+    for (int i = 1; i <= 9; i++) {
+        if (a2 % i == 0);
+    }
+}
+void kubNum(int a3) {
+    cout << pow(a3, 3);
+}
+void maxNum(int a4, int b4) {
+    if (a4 > b4)cout << a4;
+    else cout << b4;
+}
+bool num(int a5) {
+    if (a5 > 0)return true;
+    else return false;
+}
+
+
+
+
+int main()
+{   
+    num(3);
+    
+}
+
+
+
